0c539df151f32f641ebbda1e9b3e6d23d9536891
sujalchhabra <sujalchhabra91@gmail.com>
2026-08-26 22:19:31 +0530

    Modified Q2

--- DSA_lab_Assignment_1/DSA_Lab_assignment_1_Q2.cpp
+++ DSA_lab_Assignment_1/DSA_Lab_assignment_1_Q2.cpp
@@ -1,7 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
+    int arr[100];
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Removing duplicate elements
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            // Check if duplicate is found
+            if (arr[i] == arr[j])
+            {
+                // Shift elements to the left
+                for (int k = j; k < n - 1; k++)
+                {
+                    arr[k] = arr[k + 1];
+                }
+
+                n--;
+
+                // Check the same position again
+                j--;
+            }
+        }
+    }
+
+    cout << "\nArray after removing duplicates: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
